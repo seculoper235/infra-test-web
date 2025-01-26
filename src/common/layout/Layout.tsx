5b8732ac726d44843da9d214f6cfd8104a686ad4
seculoper <mir8725@naver.com>
2025-01-26 09:10:42 +0900

    Todo Space 화면 수정

--- src/common/layout/Layout.tsx
+++ src/common/layout/Layout.tsx
@@ -1,5 +1,4 @@
-import MailIcon from "@mui/icons-material/Mail"
-import InboxIcon from "@mui/icons-material/MoveToInbox"
+import {Checklist} from "@mui/icons-material"
 import {
     Box,
     Divider,
@@ -12,9 +11,28 @@ import {
     Stack,
     Toolbar
 } from "@mui/material"
-import {useOutlet} from "react-router-dom"
+import React from "react"
+import {useNavigate, useOutlet} from "react-router-dom"
 
-const drawerWidth = 240
+export const drawerWidth = 240
+
+type DrawerMenuProps = {
+    label: string
+    route: string
+    icon: React.ReactNode
+}
+const DrawerMenu = ({label, route, icon}: DrawerMenuProps) => {
+    const navigate = useNavigate()
+
+    return <ListItem key={label} disablePadding>
+        <ListItemButton onClick={() => navigate(route)}>
+            <ListItemIcon>
+                {icon}
+            </ListItemIcon>
+            <ListItemText primary={label}/>
+        </ListItemButton>
+    </ListItem>
+}
 
 export const Layout = () => {
     const outlet = useOutlet()
@@ -22,18 +40,6 @@ export const Layout = () => {
     return <>
         <Box sx={{display: "flex"}}>
             {/*Drawer*/}
-            {/*<CssBaseline />
-            <AppBar
-                position="fixed"
-                sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-            >
-                <Toolbar>
-                    <Typography variant="h6" noWrap component="div">
-                        Permanent drawer
-                    </Typography>
-                </Toolbar>
-            </AppBar>*/}
-
             <Drawer
                 sx={{
                     width: drawerWidth,
@@ -49,36 +55,16 @@ export const Layout = () => {
                 <Toolbar/>
                 <Divider/>
                 <List>
-                    {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-                        <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
-                                </ListItemIcon>
-                                <ListItemText primary={text}/>
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
+                    <DrawerMenu label={"할 일"} route={"todo"} icon={<Checklist/>}/>
                 </List>
                 <Divider/>
-                <List>
-                    {["All mail", "Trash", "Spam"].map((text, index) => (
-                        <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
-                                </ListItemIcon>
-                                <ListItemText primary={text}/>
-                            </ListItemButton>
-                        </ListItem>
-                    ))}
-                </List>
             </Drawer>
         </Box>
 
-        <Stack className={"space-content"} alignItems={"center"} justifyContent={"center"} pl={`${drawerWidth}px`}
+        {/*메인*/}
+        <Stack className={"space-content"} alignItems={"stretch"} justifyContent={"center"}
+               pl={`${drawerWidth}px`}
                height={"100vh"}>
-            {/*메인*/}
             {outlet}
         </Stack>
     </>
