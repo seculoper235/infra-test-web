dc513ba0c9f2c7cb3932e22150d64fef5ba4731b
seculoper <mir8725@naver.com>
2025-02-28 23:53:03 +0900

    Loader 구현

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import {createRoot} from "react-dom/client"
 import "./index.css"
 import {RecoilRoot} from "recoil"
 import App from "./App.tsx"
+import {LoadingProvider} from "./common/compoent/Loader.tsx"
 
 createRoot(document.getElementById("root")!).render(
     <RecoilRoot>
+        <LoadingProvider/>
         <App/>
     </RecoilRoot>
 )
